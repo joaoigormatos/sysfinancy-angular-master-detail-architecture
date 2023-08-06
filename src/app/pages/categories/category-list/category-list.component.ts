f886c84c84598cf2035846576a52a2a7ce9831dd
João Igor <joaoigormatos123@gmail.com>
2023-08-06 16:17:40 -0300

    wrapup category-list

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = []
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe((data: Category[]) => {
+      this.categories = data
+    },
+    error => alert(`Error ao carregar a lista`)
+    )
   }
 
+  deleteCategory(id: number) {
+    const mustDelete = confirm("Deseja mesmo excluir essa categoria?")
+
+    if(mustDelete) {
+      this.categoryService.delete(id).subscribe(()=> {
+        this.categories = this.categories.filter((category) => category.id !== id)
+      },
+      (error) => alert(`Error na delação da categoria`)
+      )
+    }
+  }
 }
